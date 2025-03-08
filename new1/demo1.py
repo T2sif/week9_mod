c6667f6bd2021e2c2f9c96eb89fb5f9e737c3462
T2sif <2sif.funtoos@gmail.com>
2025-03-08 10:30:25 +0530

    Added new_folder to the repository

---
+++ new1/demo1.py
@@ -0,0 +1,5 @@
+import my_pack.math_tools as mt
+import my_pack.string_tools as st
+
+print(mt.add(5, 3))  # Output: 8
+print(st.uppercase("hello"))  # Output: HELLO
---
+++ new1/my_pack/__init__.py
@@ -0,0 +1,2 @@
+from .math_tools import add, subtract
+from .string_tools import uppercase, lowercase
